3751e97664f31a2247d3817b0fa48b40b767667c
Dev Frias <ddfrias14@gmail.com>
2021-02-25 23:13:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ let currentLine = katzDeliLine => {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-    for (i = 0; i < katzDeliLine.length; i++) {
+    for (let i = 0; i < katzDeliLine.length; i++) {
       return `The line is currently ${[i + 1] + katzDeliLine}`
 }}}
